a07a21e0c64034aa93f7cd61772c54e1a1298252
CrankyUnicorn <azedopeter@gmail.com>
2025-08-21 15:14:57 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,7 +2,9 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  downloadAudio: (url, format) => ipcRenderer.send('download-audio', { url, format }),
+  downloadAudio: () => ipcRenderer.send('download-audio'),
+  addUrlQueue: (url, format) => ipcRenderer.send('add-url-queue', { url, format }),
+  queueUpdated: (callback) => ipcRenderer.on('queue-updated', (event, queueStatus) => callback(queueStatus)),
   selectDownloadFolder: () => ipcRenderer.invoke('select-download-folder'),
   getSavedFolder: () => ipcRenderer.invoke('get-saved-folder'),
   onProgress: (callback) => ipcRenderer.on('download-progress', (event, percent) => callback(percent)),
